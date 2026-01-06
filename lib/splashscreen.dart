08ca0a819e119fce8961b393ab398ad29154d663
ZTPG0389 <priya.gupta@zehntech.com>
2026-01-07 00:27:02 +0530

    Update splash screen logic

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -3,7 +3,6 @@ import 'package:chatapp/repository/screens/onboarding/onboardingscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
